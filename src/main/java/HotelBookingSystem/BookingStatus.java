831f42961e8ea6e802f1d609c8e7fa80ac037c89
JanLuiss <qdh9571@autuni.ac.nz>
2026-04-08 21:53:37 +1200

    Added Checkout Requested enum

--- src/main/java/HotelBookingSystem/BookingStatus.java
+++ src/main/java/HotelBookingSystem/BookingStatus.java
@@ -5,9 +5,11 @@ public enum BookingStatus {
     PENDING("Pending"),
     CANCELLED("Cancelled"),
     COMPLETED("Completed"),
-    CANCELLATION_REQUESTED("Cancellation Requested");
+    CANCELLATION_REQUESTED("Cancellation Requested"),
+    CHECKOUT_REQUESTED("Checkout Requested");
 
     private String status;
+    private BookingStatus stat;
 
     private BookingStatus(String status) {
         this.status = status;
@@ -16,4 +18,9 @@ public enum BookingStatus {
     public String getStatus() {
         return this.status;
     }
+    
+    public BookingStatus getBookingStatus() {
+    	return this.stat;
+    }
+    
 }

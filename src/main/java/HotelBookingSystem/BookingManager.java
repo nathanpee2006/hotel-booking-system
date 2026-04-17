15d2c47770236ca655fd522355b414dc7b487318
Jan Luiss Reyes <reyesjanluiss@gmail.com>
2026-04-17 12:23:56 +1200

    Moved methods from Booking Manager to HotelClerk

--- src/main/java/HotelBookingSystem/BookingManager.java
+++ src/main/java/HotelBookingSystem/BookingManager.java
@@ -18,21 +18,6 @@ public class BookingManager {
         return room.checkAvailability(dateRange.getStart(), dateRange.getEnd());
     }
 
-    public Booking createBooking(Customer customer, Room room, DateRange dateRange) {
-        if (!checkAvailability(room, dateRange)) {
-            throw new IllegalStateException("Room is not available");
-        }
-
-        int newId = bookingRepo.generateId();
-        Booking booking = new Booking(newId, customer, room, dateRange, BookingStatus.PENDING);
-
-        bookingRepo.save(booking);
-        room.reserve(dateRange.getStart(), dateRange.getEnd());
-        roomRepo.updateRoom(room);
-
-        return booking;
-    }
-
     public void cancelBooking(int bookingId, String customerEmail) {
         Booking booking = bookingRepo.findById(bookingId);
 
@@ -55,22 +40,6 @@ public class BookingManager {
         roomRepo.updateRoom(booking.getRoom());
     }
 
-    public void completeBooking(int bookingId) {
-        Booking booking = bookingRepo.findById(bookingId);
-
-        if (booking == null) {
-            throw new IllegalArgumentException("Booking not found");
-        }
-
-        paymentProcessor.process(booking.getAmount());
-
-        booking.completeBooking();
-        bookingRepo.update(booking);
-
-        booking.getRoom().occupy();
-        roomRepo.updateRoom(booking.getRoom());
-    }
-
     public void requestCancellation(int bookingId, String customerEmail) {
         Booking booking = bookingRepo.findById(bookingId);
 
@@ -90,31 +59,11 @@ public class BookingManager {
         bookingRepo.update(booking);
     }
 
-    public void approveCancellation(int bookingId) {
-        Booking booking = bookingRepo.findById(bookingId);
-
-        if (booking == null) {
-            throw new IllegalArgumentException("Booking not found.");
-        }
-
-        if (booking.getBookingStatus() != BookingStatus.CANCELLATION_REQUESTED) {
-            throw new IllegalStateException("Booking is not awaiting cancellation approval.");
-        }
-
-        paymentProcessor.refund(booking.getAmount());
-
-        booking.cancel();
-        bookingRepo.update(booking);
-
-        booking.getRoom().release(booking.getDateRange().getStart(), booking.getDateRange().getEnd());
-        roomRepo.updateRoom(booking.getRoom());
-    }
 
     public void requestCheckout(int bookingId, String customerEmail) {
 
         Booking booking = bookingRepo.findById(bookingId);
 
-        //I just added any possible instance(not final) baka may na-miss parin ako I'm up for some other suggestions to add...
         if (booking == null) {
             throw new IllegalArgumentException("Booking not found.");
         }
@@ -138,30 +87,6 @@ public class BookingManager {
 
         System.out.println("Checkout request submitted. A hotel clerk will confirm your checkout.");
     }
-    
-    public void confirmCheckout(int bookingId) {
 
-        Booking booking = bookingRepo.findById(bookingId);
-
-        if (booking == null) {
-            throw new IllegalArgumentException("Booking not found.");
-        }
-
-        if (booking.getBookingStatus() != BookingStatus.CHECKOUT_REQUESTED) {
-            throw new IllegalStateException(
-                "Checkout can only be confirmed for checkout requests."
-            );
-        }
-
-        booking.setBookingStatus(BookingStatus.CHECKED_OUT);
-        bookingRepo.update(booking);
-
-        Room room = booking.getRoom();
-        room.release(booking.getDateRange().getStart(), booking.getDateRange().getEnd());
-        roomRepo.updateRoom(room);
-
-        System.out.println("Checkout confirmed for booking ID: " + bookingId);
-    }
-    
 
 }
--- src/main/java/HotelBookingSystem/HotelClerk.java
+++ src/main/java/HotelBookingSystem/HotelClerk.java
@@ -1,27 +1,100 @@
 package HotelBookingSystem;
 
 public class HotelClerk extends User{
+	 	
+	private final BookingManager myManager;
+	private final IRoomRepository roomRepo;
+    private final IBookingRepository bookingRepo;
+    private final IPaymentProcessor paymentProcessor;
 	
-    public HotelClerk(int userId, String name, String email, BookingManager manager) { 
+    public HotelClerk(int userId, 
+    				String name, 
+    				String email, 
+    				BookingManager manager,
+    				IRoomRepository roomRepo,
+    	            IBookingRepository bookingRepo, 
+    	            IPaymentProcessor paymentProcessor) { 
 
         super(userId, name, email); 
-
+        this.myManager = manager;
+        this.roomRepo = roomRepo;
+        this.bookingRepo = bookingRepo;        
+        this.paymentProcessor = paymentProcessor;
     } 
+    
+    public Booking createBooking(Customer customer, Room room, DateRange dateRange) {
+        if (myManager.checkAvailability(room, dateRange)) {
+            throw new IllegalStateException("Room is not available");
+        }
 
-    // TODO: will consider removing methods 
+        int newId = bookingRepo.generateId();
+        Booking booking = new Booking(newId, customer, room, dateRange, BookingStatus.PENDING);
 
-    public void confirmCheckout() { 
+        bookingRepo.save(booking);
+        room.reserve(dateRange.getStart(), dateRange.getEnd());
+        roomRepo.updateRoom(room);
 
-         
+        return booking;
+    }
 
-    } 
+    public void completeBooking(int bookingId) {
+        Booking booking = bookingRepo.findById(bookingId);
 
-     
+        if (booking == null) {
+            throw new IllegalArgumentException("Booking not found");
+        }
 
-    public void viewSchedule() { 
+        paymentProcessor.process(booking.getAmount());
 
-         
+        booking.completeBooking();
+        bookingRepo.update(booking);
 
-    } 
+        booking.getRoom().occupy();
+        roomRepo.updateRoom(booking.getRoom());
+    }    
 
+    public void approveCancellation(int bookingId) {
+        Booking booking = bookingRepo.findById(bookingId);
+
+        if (booking == null) {
+            throw new IllegalArgumentException("Booking not found.");
+        }
+
+        if (booking.getBookingStatus() != BookingStatus.CANCELLATION_REQUESTED) {
+            throw new IllegalStateException("Booking is not awaiting cancellation approval.");
+        }
+
+        paymentProcessor.refund(booking.getAmount());
+
+        booking.cancel();
+        bookingRepo.update(booking);
+
+        booking.getRoom().release(booking.getDateRange().getStart(), booking.getDateRange().getEnd());
+        roomRepo.updateRoom(booking.getRoom());
+    }
+
+    public void confirmCheckout(int bookingId) {
+
+        Booking booking = bookingRepo.findById(bookingId);
+
+        if (booking == null) {
+            throw new IllegalArgumentException("Booking not found.");
+        }
+
+        if (booking.getBookingStatus() != BookingStatus.CHECKOUT_REQUESTED) {
+            throw new IllegalStateException(
+                "Checkout can only be confirmed for checkout requests."
+            );
+        }
+
+        booking.setBookingStatus(BookingStatus.CHECKED_OUT);
+        bookingRepo.update(booking);
+
+        Room room = booking.getRoom();
+        room.release(booking.getDateRange().getStart(), booking.getDateRange().getEnd());
+        roomRepo.updateRoom(room);
+
+        System.out.println("Checkout confirmed for booking ID: " + bookingId);
+    }
+    
 }

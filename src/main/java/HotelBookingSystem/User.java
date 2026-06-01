bf800d62b61d0e2dfa4816134b993f7377faba51
Jan Luiss Reyes <reyesjanluiss@gmail.com>
2026-06-01 21:40:06 +1200

    Few modifications, and added extra methods

--- src/main/java/HotelBookingSystem/User.java
+++ src/main/java/HotelBookingSystem/User.java
@@ -6,13 +6,15 @@ public abstract class User {
     private String name;
     private String email;
     private UserRole role;
+    private String password;
 
     // Used by authenticated users (from DB)
-    public User(int userId, String name, String email, UserRole role) {
+    public User(int userId, String name, String email, UserRole role, String password) {
         this.userId = userId;
         this.name = name;
         this.email = email;
         this.role = role;
+        this.password = password;
     }
 
     // Kept for backward compatibility (CUI / lightweight construction)
@@ -21,6 +23,14 @@ public abstract class User {
         this.name = name;
         this.email = email;
         this.role = null;
+    }
+    
+    public User(String name, String email, String password, UserRole role) {
+        this.userId = -1; 
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
     }
 
     public int getUserId() {
@@ -34,6 +44,10 @@ public abstract class User {
     public String getEmail() {
         return email;
     }
+    
+    public String getPasswrod() {
+        return password;
+    }
 
     public UserRole getRole() {
         return role;
@@ -41,5 +55,9 @@ public abstract class User {
 
     public boolean isAuthenticated() {
         return userId != -1;
+    }
+    
+    public void setRole(UserRole role){
+        this.role = role;
     }
 }
